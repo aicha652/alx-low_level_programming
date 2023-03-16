f4bffb4bca68b4fc3e329c4d4d2f638116f4ae51
aicha652 <aichaazlf652@gmail.com>
2023-03-16 16:44:59 -0700

    add files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Entry piont
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{        
+	char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	putchar(ch);
+	return (1);
+}
