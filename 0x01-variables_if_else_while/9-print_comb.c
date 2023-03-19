dcd46545f4c3d513922d79430a5615e4d4e3b333
aicha652 <aichaazlf652@gmail.com>
2023-03-19 05:31:38 -0700

    add 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,22 @@
  */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for(i = 0; i < 10; i++)
+	for(; i < 10;)
 	{
-		putchar('0' + i);
-		putchar(',');
-		putchar(' ');
+		if(i != 9)
+		{
+			putchar('0' + i);
+			putchar(',');
+		        putchar(' ');
+			i++;
+		}
+		else
+		{
+			putchar('0' + i);
+			i++;
+		}
 	}
 	putchar('\n');
 	return (0);
