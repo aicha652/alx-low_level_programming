c8c0454457e60f7f8fd96cd319df9ffad6d3f546
aicha652 <aichaazlf652@gmail.com>
2023-03-20 07:08:43 -0700

    add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ for (i = 0; i < 9; i++)
 	for (j = i + 1; j < 10; j++)
 	{
 		putchar(i + '0');
-                putchar(j + '0');
+		putchar(j + '0');
 		if (i == 8 && j == 9)
 		{
 			continue;
 		}
 		putchar(',');
-                putchar(' ');
+		putchar(' ');
 	}
 }
 putchar('\n');
