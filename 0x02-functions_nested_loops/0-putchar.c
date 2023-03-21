64fcd2e1414da656a55ee0d9c2f82c41bcd20c89
aicha652 <aichaazlf652@gmail.com>
2023-03-21 13:56:50 -0700

    add 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,10 @@
  * File 0-putchar.c
  */
 #include "main.h"
-/**
- * main - prints _putchar followed by new line
+/*
+ * main - Prints "_putchar" followed by a new line.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
