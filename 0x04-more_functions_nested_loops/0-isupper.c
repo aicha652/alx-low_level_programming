ad5d9367690bc56072ad13a8381041ae725c51d9
aicha652 <aichaazlf652@gmail.com>
2023-03-23 07:57:37 -0700

    add files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase character
+ *@c: the character to be checked
+ *Return: 1 Success
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
