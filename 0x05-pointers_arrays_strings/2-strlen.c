f5b510b0148932ceac43dba032792b689ed3dff1
aicha652 <aichaazlf652@gmail.com>
2023-03-27 08:47:18 -0700

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s:  the length of a string
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	size_t x;
+	
+	x = strlen(s);
+	_putchar(x + '0');
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#include <string.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+size_t strlen(const char *s);
