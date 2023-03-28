82ec8bfe839dd643dac6877a956043af3e48b5a8
aicha652 <aichaazlf652@gmail.com>
2023-03-28 07:34:08 -0700

    add File

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * rev_string - function that reverses a string
- * @s : the string to be reverse
- * Return: void
- */
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
 void rev_string(char *s)
 {
 	int i, c, k;
