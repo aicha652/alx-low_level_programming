c71c5aeab9413f9d2e72ac8992af6d4d8af999b7
aicha652 <aichaazlf652@gmail.com>
2023-03-28 06:39:13 -0700

    add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: thestring to be converts
+ * Return: intger
+ */
+int _atoi(char *s)
+{
+int val = atoi(s);
+return (val);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,3 +14,5 @@ void rev_string(char *s);
 void puts2(char *str);
 char *_strcpy(char *dest, char *src);
 void puts_half(char *str);
+int _atoi(char *s);
+int atoi (const char *__nptr);
