d55ee98b68d0283a564ef5254c4b8ff30e2a6150
aicha652 <aichaazlf652@gmail.com>
2023-03-30 06:54:08 -0700

    add 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-/*size_t dest_len = strlen(dest);
-size_t i;
-
-for (i = 0 ; i < strlen(src) && src[i] != '\0' ; i++)
-{
-	dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
-}*/
 printf("%s", strcat(dest, src));
 return (dest);
 }
