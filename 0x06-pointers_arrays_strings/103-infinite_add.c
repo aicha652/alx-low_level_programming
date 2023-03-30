55e624920777c76859908aab43dde6976fee0e4f
aicha652 <aichaazlf652@gmail.com>
2023-03-30 07:52:20 -0700

    add File

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,9 +7,11 @@
  * @size_r: the buffer size
  * Return: pointer to the result
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, size_t size_r)
 {
-	r = n1 + n2;
+	*r = *n1 + *n2;
 	printf("%s", r);
+	if (size_r < strlen(n1) || size_r < strlen(n2))
+		return (0);
 	return (0);
 }
