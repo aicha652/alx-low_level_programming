e42c60441b6a41dcadbc6e43179b910a9ecfc6bd
aicha652 <aichaazlf652@gmail.com>
2023-03-30 05:48:31 -0700

    add 102-magic.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ * @str: string to be encode
+ * Return: string
+ */
+char *leet(char *str)
+{
+	size_t i;
+
+	for(i = 0; i < strlen(str); i++)
+	{
+	       	if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = 4;
+			printf("%u", str[i]);
+		}
+	        else if(str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = 3;
+			printf("%u", str[i]);
+		}
+	        else if(str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = 0;
+			printf("%u", str[i]);
+		}
+		else if(str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = 7;
+			printf("%u", str[i]);
+		}
+		else if(str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = 1;
+			printf("%u", str[i]);
+		}
+		else
+		{
+			printf("%u", str[i]);
+		}
+	}
+	printf("%s", str);
+	printf("\n");
+	return (str);
+}
