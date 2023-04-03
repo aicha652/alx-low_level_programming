d798c364672c8fec9efa2873aef45a91230b084f
aicha652 <aichaazlf652@gmail.com>
2023-04-03 05:45:21 -0700

    add 1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #include <stdio.h>
 char *_memset(char *s, char b, unsigned int n);
 void *memset(void *s, int b, size_t n);
-int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *memcpy(void *dest, const void *src, size_t n);
