5efb8ad6e33ac5b9d1c7f9b6b379b5137b53df7a
aicha652 <aichaazlf652@gmail.com>
2023-04-03 07:15:06 -0700

    add 3-strspn.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 void *memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 char *strchr(const char *s, int c);
+unsigned int _strspn(char *s, char *accept);
+size_t strspn(const char *s, const char *accept);
