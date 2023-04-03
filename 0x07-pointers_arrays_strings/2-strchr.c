d84220f717b12414d7a863552b64955b67b1e8ac
aicha652 <aichaazlf652@gmail.com>
2023-04-03 06:56:22 -0700

    add 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,7 @@
  */
 char *_strchr(char *s, char c)
 {
-char *ch = strrchr(s, c);
+char *ch = strchr(s, c);
 
 if (ch)
 	return (ch);
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,4 @@ void *memset(void *s, int b, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void *memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
-char *strrchr(const char *s, int c);
+char *strchr(const char *s, int c);
