41ddb185fa711d0238632cd3e8a6127251356cfd
aicha652 <aichaazlf652@gmail.com>
2023-04-04 15:50:27 -0700

    add 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+ * print_chessboard - function that prints the print_chessboard
+ * @a: pointer
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for  (j = 0; j < 8; j++)
+		{
+			if ( a[i][j] == ' ')
+			{
+				continue;
+			}
+			else
+			{
+				printf("%c", a[i][j]);
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+	
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,3 +13,5 @@ char *strpbrk(const char *s, const char *accept);
 char *_strstr(char *haystack, char *needle);
 char *strstr(const char *haystack, const char *needle);
 void set_string(char **s, char *to);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
