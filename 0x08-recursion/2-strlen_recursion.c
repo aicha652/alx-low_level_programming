6d6b24f44c6296e5f7adf0950df8dbe235d019ae
aicha652 <aichaazlf652@gmail.com>
2023-04-05 07:00:31 -0700

    add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	size_t i;
+
+	i = strlen(s);
+	return (i);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 void _puts_recursion(char *s);
 int puts(const char *s);
 int _putchar(char c);
+int _strlen_recursion(char *s);
+size_t strlen(const char *s);
