b739b06c42062e13b279d629f9d901879f0ee852
aicha652 <aichaazlf652@gmail.com>
2023-04-05 10:36:35 -0700

    add 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<math.h>
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of y
+ * @x: First int
+ * @y: second int
+ * Return: value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (pow(x, y));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 void _puts_recursion(char *s);
 int puts(const char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
 size_t strlen(const char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+double pow(double x, double y);
