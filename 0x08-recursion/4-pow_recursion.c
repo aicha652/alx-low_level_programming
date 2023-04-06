b68e29a946fa7891134a7e6cb3eda282e4448932
aicha652 <aichaazlf652@gmail.com>
2023-04-06 05:16:23 -0700

    add Files

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <math.h>
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that returns the value of x
  * @x: First int
  * @y: second int
  * Return: value of x raised to the power of y
