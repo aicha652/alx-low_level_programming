3c7c3f97a3b9fb6b1186799082f792a37027c002
aicha652 <aichaazlf652@gmail.com>
2023-04-06 05:14:23 -0700

    add Files

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,15 +8,16 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int i;
-
+	if (y == 0)
+	{
+		return (1);
+	}
 	if (y < 0)
 	{
 		return (-1);
 	}
 	else
 	{
-		i = pow(x, y);
-		return (i);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
