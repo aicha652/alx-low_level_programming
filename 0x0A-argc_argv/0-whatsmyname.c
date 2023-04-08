a1dd0c92d2aa74b5cf125535756d19687e97605d
aicha652 <aichaazlf652@gmail.com>
2023-04-08 04:26:26 -0700

    add file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * program that prints its name followed by a new line
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%s \n", argv[0]);
+	printf("%s\n", argv[0]);
 	(void)argc;
 	return (0);
 }
