32fc5a10d944d325c7d8b7f7418b6f12c7c0c4bf
aicha652 <aichaazlf652@gmail.com>
2023-04-08 07:25:02 -0700

    add 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that adds positive numbers
+ * @argc: number of line comman arguments
+ * @argv: array of size argc
+ * Return: addition
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+
+if (argc == 1)
+{
+	printf("0\n");
+}
+else
+{
+	for (i = 1; i < argc;)
+	{
+		char *c;
+
+		sum = sum + strtol(argv[i], &c, 10);
+		if (c == argv[i])
+		{
+			printf("Error\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%d\n", sum);
+}
+return (0);
+}
