7393c0c5d9e2d6cab6bc3a813e4788ee967e21d4
aicha652 <aichaazlf652@gmail.com>
2023-04-10 06:48:56 -0700

    add 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,16 +11,12 @@ int main(int argc, char *argv[])
 int i, coin = 0;
 
 if (argc != 2)
-{
 	printf("Error\n");
 	return (1);
-}
 i = atoi(argv[1]);
 if (i < 0)
-{
 	printf("0\n");
 	return (0);
-}
 for (; i >= 0;)
 {
 	if (i >= 25)
