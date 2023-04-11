1f8a2b449d5db122a05497d87d76cfd882e34002
aicha652 <aichaazlf652@gmail.com>
2023-04-11 16:19:38 -0700

    add files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s =  malloc(strlen(s1) + strlen(s2) + 1);
+	char *s =  malloc((strlen(s1) + strlen(s2) + 1)*sizeof(char));
 
 	if (s1 == NULL && s2 != NULL)
 	{
@@ -17,11 +17,21 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (s1);
 	}
+	else if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
 	else
 	{
-		(void)s;
-		strcat(s1, s2);
-		return (s1);
+		s = strcat(s1, s2);
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			return (s);
+		}
 	}
 	return (0);
 
