01208395d7db1ab3306689926527fa59eb60143e
aicha652 <aichaazlf652@gmail.com>
2023-04-11 09:45:42 -0700

    add files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 char *s;
 unsigned int i;
 
-s = (char*) malloc(size * sizeof(char));
+s = (char *) malloc(size * sizeof(char));
 if (size == 0)
 {
 	return (NULL);
