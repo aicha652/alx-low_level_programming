35b28c4627f8b86024ab6bf28e90b1d413c3a294
aicha652 <aichaazlf652@gmail.com>
2023-04-12 05:16:25 -0700

    add files

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * **alloc_grid - function that returns a pointer
+ * @width: width of the array
+ * @height: height of the array
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, k[100];
+	/*k = malloc (width * height * sizeof(int));*/
+
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			k [j] = 0;
+			printf("%d ", k[j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return (0);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,3 +8,4 @@ char *_strdup(char *str);
 char *strdup(const char *s);
 char *str_concat(char *s1, char *s2);
 char *strcat(char *s1, const char *s2);
+int **alloc_grid(int width, int height);
