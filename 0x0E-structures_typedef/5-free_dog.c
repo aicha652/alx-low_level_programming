9abe5c5a718cf950abbd5a75cc26d6ad4243842c
aicha652 <aichaazlf652@gmail.com>
2023-04-17 14:38:17 -0700

    add files

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to the struct
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
