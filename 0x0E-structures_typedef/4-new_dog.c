0b87c58bb3dbc01b93f92a4cad79f87a121f0371
aicha652 <aichaazlf652@gmail.com>
2023-04-17 11:07:50 -0700

    add files

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	name = name;
+	age = age;
+	owner = owner;
+	return (0);
+}
