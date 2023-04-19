0c9726d7f1285d62c08be72bdfa3e68b49e4ed99
aicha652 <aichaazlf652@gmail.com>
2023-04-19 06:39:33 -0700

    add files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,10 +9,10 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(void)name;
-	f = print_name_as_is;
+	f(name);
+	/* f = print_name_as_is;
 	(*f)("Bob");
 	f = print_name_uppercase;
 	(*f)("Bob Dylan");
-	exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS); */
 }
