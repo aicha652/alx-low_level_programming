8cab601cbe2da00a2ff0b262706647cd5c8953f2
aicha652 <aichaazlf652@gmail.com>
2023-04-19 07:32:59 -0700

    add files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -5,5 +6,6 @@ void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
