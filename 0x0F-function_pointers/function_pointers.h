21d906c9925112c8fd79e1ef92a4c8c31588adc3
aicha652 <aichaazlf652@gmail.com>
2023-04-19 06:36:06 -0700

    add files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
