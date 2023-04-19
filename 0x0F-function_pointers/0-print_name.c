01e08b7366ebe18ec18c26ca3a319736c8c5bdb4
aicha652 <aichaazlf652@gmail.com>
2023-04-19 06:25:37 -0700

    add files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
 /**
  * print_name - function that prints a name
  * @name: name of the person
+ * @f: pointer to the functions
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
