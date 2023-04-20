55984fcc9437a658b7e0751d3a03f26b6aff6617
aicha652 <aichaazlf652@gmail.com>
2023-04-20 08:22:28 -0700

    add files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 /**
  * sum_them_all - function that returns the sum of all its parameters
- * @n: variable
+ * @n: number of parametrs
  * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
