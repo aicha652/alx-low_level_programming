16835d07056add5c6a29ca0dac03daac4b3ca233
aicha652 <aichaazlf652@gmail.com>
2023-04-26 04:20:36 -0700

    add filex

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,11 +8,14 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
+	char *ptr;
+	ptr = malloc(sizeof(*s1) + sizeof(*s2)*n);
 
 	for (i = 0; i < n && s2[i] != '\0'; i++)
 	{
 		s1[strlen(s1) + i] = s2[i];
 	}
 	s1[strlen(s1) + i] = '\0';
-	return 0;
+
+	return (ptr);
 }
