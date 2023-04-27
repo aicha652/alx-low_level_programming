a479f0ad0236426de59fe62af305c43ddeeaca96
aicha652 <aichaazlf652@gmail.com>
2023-04-27 14:58:07 -0700

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	new->str = (char *)str;
+	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
