94155230612d8127b66d2c9ee3d039e597342f2a
aicha652 <aichaazlf652@gmail.com>
2023-04-27 06:46:50 -0700

    fix error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,7 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdio.h>
 /**
  * print_list - function that prints all the elements of a list_t list
- * @h: poinet to the list
+ * @h: the list
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
