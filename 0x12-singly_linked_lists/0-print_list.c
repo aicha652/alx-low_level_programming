c48f4760c291611b98b0e117599f7e6d5f7cce03
aicha652 <aichaazlf652@gmail.com>
2023-04-27 06:16:08 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,11 +10,15 @@ size_t print_list(const list_t *h)
 {
 	unsigned int n = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%u] (nil) \n", h->len);
+			printf("[0] (nil)\n");
 		}
 		else
 		{
