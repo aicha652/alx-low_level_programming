b0a72edd010c7a6062377789d00c873800590ba2
aicha652 <aichaazlf652@gmail.com>
2023-04-27 16:19:57 -0700

    fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * print_func - function that prints specific sentence before the main is executed.
+ * print_before - function that prints before main
  *
  * Return: sentence
  */
-void print_before() __attribute__((constructor));
-void print_before()
+void print_before(void) __attribute__((constructor));
+void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
