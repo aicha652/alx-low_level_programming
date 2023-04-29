b009c973b7b8aaa32412c72a9a9feea1a68d94ff
aicha652 <aichaazlf652@gmail.com>
2023-04-29 05:21:12 -0700

    fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,14 +8,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *list;
 
-	if (head == NULL)
-	{
-	}
-
 	while (head != NULL)
 	{
 		list = head;
-		free(head);
-		head = list->next;
+		head = head->next;
+		free(list);
 	}
 }
