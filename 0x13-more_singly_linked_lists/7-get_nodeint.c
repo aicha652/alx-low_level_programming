820c41b701d224e3b7a3702d75e83537c2e8dfba
aicha652 <aichaazlf652@gmail.com>
2023-04-30 05:51:38 -0700

    fix error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
-	return (head);
-
-	return (NULL);
+	if (head)
+		return (head);
+	else
+		return (NULL);
 }
