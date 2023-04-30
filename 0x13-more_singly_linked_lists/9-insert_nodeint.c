64e0deb740d1766280329d705506276563a3e17e
aicha652 <aichaazlf652@gmail.com>
2023-04-30 15:09:50 -0700

    fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,16 +15,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new = malloc((sizeof(listint_t)));
 	list = *head;
-	while (list && i < idx)
-	{
-		i++;
-		list = list->next;
-	}
-	if (!*head)
-		return (NULL);
-	new->n = n;
-	new->next = list->next;
-	list->next = new;
 
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	else
+	{
+		while (list && i < idx)
+		{
+			i++;
+			list = list->next;
+		}
+		if (!*head)
+			return (NULL);
+		new->n = n;
+		new->next = list->next;
+		list->next = new;
+	}
 	return (*head);
 }
