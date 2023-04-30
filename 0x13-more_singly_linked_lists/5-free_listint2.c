5a1958a58cd662ea4eaf827456269884cdbe7d06
aicha652 <aichaazlf652@gmail.com>
2023-04-30 04:32:34 -0700

    fix error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,8 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *list;
 	while (*head != NULL)
 	{
-		*head = NULL;
+		list = *head;
+		*head = (*head)->next;
+		free(list);
 	}
+	*head = NULL;
 }
