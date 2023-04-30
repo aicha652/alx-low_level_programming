7582a0fa2a96d88419182c13da0b1ebaa8ea1dd7
aicha652 <aichaazlf652@gmail.com>
2023-04-30 04:10:31 -0700

    add files

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: list
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+	while (*head != NULL)
+	{
+		list = *head;
+		*head = (*head)->next;
+		free(list);
+	}
+	*head = NULL;
+}
