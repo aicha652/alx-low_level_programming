9816b5030a9da4af9b191cefdf6a0040acd11dbc
aicha652 <aichaazlf652@gmail.com>
2023-05-01 04:52:54 -0700

    fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			list = list->next;
 			i++;
 		}
-		if (!list)
+		if (list == NULL)
 			return (NULL);
 		new->n = n;
 		new->next = list->next;
