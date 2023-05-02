cd3065449a6666e48a6b9299ae37b26e56202ed8
aicha652 <aichaazlf652@gmail.com>
2023-05-02 05:29:07 -0700

    fix error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,9 +8,6 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 
-	if (head == NULL)
-		return (0);
-
 	while (head != NULL)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
