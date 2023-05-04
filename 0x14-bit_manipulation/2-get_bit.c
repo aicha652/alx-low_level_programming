40fedde1a5def3474bac0e5da341786979ad5594
aicha652 <aichaazlf652@gmail.com>
2023-05-04 11:35:17 -0700

    fix error

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: integer
+ * @index: index of the bit to be obtained
+ * Return: the value of a bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (n 
