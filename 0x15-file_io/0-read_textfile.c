64183aa7daa48097352ef3177076e9eab475964b
aicha652 <aichaazlf652@gmail.com>
2023-05-08 08:10:27 -0700

    fix error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * read_textfile -  function that reads a text file and prints it to the POSIX standard output
+ * read_textfile -  function that reads a text file
+ * and prints it to the POSIX standard output
  * @filename: pointer to the file name
  * @letters: the number of letters it should read and print
  * Return: the number of letters
@@ -8,7 +9,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	size_t i, count = 0;
+	size_t i, res, count = 0;
 	char *buff;
 
 	buff = malloc(sizeof(char) * letters);
@@ -23,5 +24,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	for (i = 0; i < letters && buff[i] != '\0'; i++)
 		count++;
 
+	res = write(STDOUT_FILENO, buff, count);
+	if (res != count)
+		return (0);
+	close(fd);
 	return (count);
 }
