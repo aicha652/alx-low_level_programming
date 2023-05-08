e6980fffa07bb6cdf4cdb7906341f4982d1d906a
aicha652 <aichaazlf652@gmail.com>
2023-05-08 11:48:40 -0700

    add file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		write(fd, NULL, 0);
-		close (fd);
+		size_t len = strlen(text_content);
+		size_t bytes_written = write(fd, text_content, len);
+
+		if (bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-	else
-	{
-		write(fd, &text_content, strlen(text_content));
-		close (fd);
-	}
+	close(fd);
 	return (1);
 }
