0b50dc31d91a505fa635aa33fc0dde00e48843fb
aicha652 <aichaazlf652@gmail.com>
2023-05-09 07:02:37 -0700

    fix error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define BUFFER_SIZE 1024
 /**
  * main - program that copies the content of a file to another file
  * @ac: number of command line argument
@@ -9,7 +10,6 @@ int main(int ac, char **av)
 {
 	int fd, fdw, sz_read, sz_write;
 	char *buff;
-	size_t len;
 
 	if (ac != 3)
 	{
@@ -17,16 +17,15 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
-	buff = malloc(sizeof(char) * 1024);
-	len = strlen(buff);
-	sz_read = read(fd, buff, len);
+	buff = malloc(BUFFER_SIZE);
+	sz_read = read(fd, buff, BUFFER_SIZE);
 	if (fd == -1 || sz_read == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fdw = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	sz_write = write(fdw, buff, len);
+	sz_write = write(fdw, buff, BUFFER_SIZE);
 	if (fdw == -1 || sz_write == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
