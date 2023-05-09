222aa6db1a0ec3987b5f579b6a2f3eabe9297dec
aicha652 <aichaazlf652@gmail.com>
2023-05-09 07:14:28 -0700

    fix error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
