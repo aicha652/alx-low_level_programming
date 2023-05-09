dd5cdbec691ea17cbf34ab6f2e41e57554a3c45a
aicha652 <aichaazlf652@gmail.com>
2023-05-09 08:02:47 -0700

    fix error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,28 +17,18 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	buff = malloc(BUFFER_SIZE);
 	sz_read = read(fd, buff, BUFFER_SIZE);
 	if (buff == NULL)
 		return (0);
-	if (sz_read == -1)
+	if (fd == -1 || sz_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fdw = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdw == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	sz_write = write(fdw, buff, sz_read);
-	if (sz_write == -1)
+	if (fdw == -1 || sz_write == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
