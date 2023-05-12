e2cc928a6459db843ec4aab4a23d62330d7cffbb
aicha652 <aichaazlf652@gmail.com>
2023-05-12 15:07:59 -0700

    fix error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int ac, char **av)
 {
-	int fd, fdw, sz_read, sz_write, close_f;
+	int fd, fdw, sz_read, sz_write;
 	char *buff;
 
 	if (ac != 3)
@@ -17,34 +17,36 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
-	buff = malloc(BUFFER_SIZE);
-	if (buff == NULL)
-		return (0);
-	sz_read = read(fd, buff, BUFFER_SIZE);
-	if (fd == -1 || sz_read == -1)
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fdw = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	sz_write = write(fdw, buff, sz_read);
-	if (fdw == -1 || sz_write == -1)
+	if (fdw == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	close_f = close(fd);
-	if (close_f == -1)
+	buff = malloc(sizeof(char) * BUFFER_SIZE);
+	if (buff == NULL)
+		return (0);
+	while((sz_read = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
+		sz_write = write(fdw, buff, sz_read);
+		if (sz_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
 	}
-	close_f = close(fdw);
-	if (close_f == -1)
+	if (sz_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fdw %d\n", fdw);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	free(buff);
+	close(fd);
+	close(fdw);
 	return (0);
 }
