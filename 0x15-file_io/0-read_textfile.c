b4b0401da8ca33ad35d8e840896945207784da58
aicha652 <aichaazlf652@gmail.com>
2023-07-10 12:05:21 -0700

    fix error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,12 +9,26 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	ssize_t sz_read, sz_write;
+	char *buff;
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	write(fd, filename, letters);
+
+	sz_read = read(fd, buff, letters);
+	if (sz_read == -1)
+		return (0);
+
+	sz_write = write(STDOUT_FILENO, buff, letters);
 	if (filename == NULL)
+		return (0);
+	if (sz_write == -1)
 		return (0);
 	close(fd);
 	return (letters);
