44ec28dc012a5ed2e91258efd868904d6c81baff
aicha652 <aichaazlf652@gmail.com>
2023-07-10 13:30:41 -0700

    fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
 
+	if (fd == -1)
+		return (-1);
+
 	sz_write = write(fd, text_content, strlen(text_content));
 
 	if (sz_write == -1)
