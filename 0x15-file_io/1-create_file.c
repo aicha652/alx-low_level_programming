aa2f4949dd60df3ff44d3b3c50c31d73c0f5c73b
aicha652 <aichaazlf652@gmail.com>
2023-07-12 05:34:16 -0700

    fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	sz_write = write(fd, text_content, strlen(text_content));
-	if (sz_write == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		sz_write = write(fd, text_content, strlen(text_content));
+		if (sz_write == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
