cca042cbcd972ea3c611f71c9bc0416561278521
aicha652 <aichaazlf652@gmail.com>
2023-09-07 09:58:49 -0700

    fix error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,27 +8,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i, j;
+	unsigned long int i;
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
+		return (NULL);
+	}
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
 		free(table);
 		return (NULL);
 	}
-	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
-
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
-	for (j = 0; j < table->size; j++)
-	{
-		free(table->array[j]);
-	}
-	free(table->array);
-	free(table);
+	table->size = size;
 	return (table);
 }
