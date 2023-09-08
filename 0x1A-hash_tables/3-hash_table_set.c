a705665c1fd124aeb85c45c78de140d8213a5a16
aicha652 <aichaazlf652@gmail.com>
2023-09-08 13:30:03 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,9 +10,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, i = 0;
 	const unsigned char *convert_key = (const unsigned char *)key;
+	hash_node_t *node;
 
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
 	index = hash_djb2(convert_key) % ht->size;
-	while (i < ht->size)
+	while (i < ht->size && ht != NULL)
 	{
 		if (ht->array[index] == NULL)
 		{
@@ -23,14 +27,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ht->array[index]->value = strdup(value);
 			return (1);
 		}
-		if (strcmp(ht->array[index]->key, key) == 0)
+		else
+		{
+			node->key = strdup(key);
+			node->value = strdup(value);
+			node->next = ht->array[index]->next;
+			ht->array[index]->next = node;
+			return (1);
+		}
+		/*if (strcmp(ht->array[index]->key, key) == 0)
 		{
 			free(ht->array[index]->value);
 			ht->array[index]->value = strdup(value);
 			return (1);
 		}
 		index = (index + 1) % ht->size;
-		i++;
+		i++;*/
 	}
 	return (0);
 }
