f0ffd334b094263e81d00585bb4d0c8152a5ff70
aicha652 <aichaazlf652@gmail.com>
2023-09-08 12:57:28 -0700

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,19 +9,18 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i = 0;
 	hash_node_t *node;
 
-	while (ht != NULL)
+	while (i < ht->size)
 	{
-		if (ht->array[i] != NULL)
+		while (ht->array[i] != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				ht->array[i] = node->next;
-				free(node);
-				node = node->next;
-			}
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
 			free(ht->array[i]);
-			i++;
+			ht->array[i] = node;
 		}
+		i++;
 	}
+	free(ht->array);
+	free(ht);
 }
