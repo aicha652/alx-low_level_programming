5d2ff9915520e0b90d78e189c2f60bd78d95a5b4
aicha652 <aichaazlf652@gmail.com>
2023-09-08 11:48:47 -0700

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,14 +9,25 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i = 0;
+	hash_node_t *node;
 
 	while (i < ht->size)
 	{
 		if (ht->array[i] != NULL && strcmp(ht->array[i]->key, key) == 0)
 		{
 			return (ht->array[i]->value);
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (strcmp(node->key, key) == 0)
+				{
+					return (node->value);
+				}
+				node = node->next;
+			}
 		}
 		i++;
 	}
+
 	return (NULL);
 }
